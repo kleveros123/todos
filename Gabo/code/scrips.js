3a579a6ccb1aac42e1a0fe93c43636b47c86cb79
Gabo <gabboriosrosas.2@gmail.com>
2025-02-06 05:56:04 -0500

    Cambios en la estética del código

--- Gabo/code/scrips.js
+++ Gabo/code/scrips.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.querySelector('.carousel-wrapper');
     let scrollAmount = 0;
-    const cardWidth = document.querySelector('.card').offsetWidth + 15; // Ancho de la card + margen
+    const cardWidth = document.querySelector('.card').offsetWidth + 15; 
   
-    // Función para mover el carrusel
     window.moveCarousel = function(direction) {
       const containerWidth = container.scrollWidth;
       const containerVisibleWidth = container.clientWidth;
@@ -12,22 +11,21 @@ document.addEventListener("DOMContentLoaded", function () {
         if (scrollAmount < containerWidth - containerVisibleWidth) {
           scrollAmount += cardWidth;
         } else {
-          scrollAmount = 0; // Si llega al final, vuelve al inicio
+          scrollAmount = 0; 
         }
       } else if (direction === 'prev') {
         if (scrollAmount > 0) {
           scrollAmount -= cardWidth;
         } else {
-          scrollAmount = containerWidth - containerVisibleWidth; // Si está al inicio, va al final
+          scrollAmount = containerWidth - containerVisibleWidth; 
         }
       }
   
-      container.style.transform = `translateX(-${scrollAmount}px)`; // Usamos transform para mover el carrusel
+      container.style.transform = `translateX(-${scrollAmount}px)`; 
     }
   
-    // Movimiento automático cada 5 segundos
     setInterval(function() {
       moveCarousel('next');
-    }, 5000); // Mueve cada 5 segundos
+    }, 5000);
   });
   
